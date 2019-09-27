c8955b8cf0b8a2364ec834b7b28577999826db74
Andrew English <aenglish@diligent.com>
2019-09-26 22:03:50 -0400

    Setup Webpack to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-  entry: "./src/bootstrap.tsx",
-  output: {
-    filename: "./public/bundle.js"
-  },
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "eval",
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
-  },
-  module: {
-    rules: [
-      // Handle .ts and .tsx file via ts-loader.
-      { test: /\.tsx?$/, loader: "ts-loader" }
-    ]
-  }
-};
+    entry: './src/bootstrap.tsx',
+    output: {
+        filename: './bundle.js',
+    },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: 'eval',
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+    },
+    module: {
+        rules: [
+            // Handle .ts and .tsx file via ts-loader.
+            { test: /\.tsx?$/, loader: 'ts-loader' },
+        ],
+    },
+    plugins: [
+        // New plugin
+        new HtmlWebpackPlugin(),
+    ],
+}
