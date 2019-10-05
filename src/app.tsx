38d93aed59a9871dedd7ae3877fe92d15b0e0815
Andrew English <aenglish@diligent.com>
2019-10-05 11:42:57 -0400

    Added Github Corner

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,27 @@
 import { h, Component } from 'preact'
 import { css } from 'emotion'
 import { TypingContainer } from './components/TypingContainer'
+import { GithubCorner } from './components/GithubCorner'
 
 export const App = () => {
     return (
-        <div
-            className={css`
-                display: flex;
-                flex-flow: column;
-                justify-conent: center;
-                align-items: center;
-            `}
-        >
-            <h1>Simple Typing Tutor</h1>
-            <TypingContainer />
+        <div>
+            <GithubCorner
+                link="https://github.com/EnglishAW/typing-trainer"
+                fill="#64CEAA"
+                logoColor="#292d3f"
+            />
+            <div
+                className={css`
+                    display: flex;
+                    flex-flow: column;
+                    justify-conent: center;
+                    align-items: center;
+                `}
+            >
+                <h1>Simple Typing Tutor</h1>
+                <TypingContainer />
+            </div>
         </div>
     )
 }
